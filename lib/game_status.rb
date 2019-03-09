2ab0fe16940a3ec2297671cc206cbfaf0676f3ef
Ay Has <ahashim983@yahoo.com>
2019-03-09 17:44:35 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -22,8 +22,7 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |single_combination|
-    puts single_combination
-    single_combination.select{|i| board[i].match("X")}
+    single_combination.select{|i| board[i].include?("X")}
 
 
 
