0e5039b82a9ad0b28c75fe0bdedb28e82b03c1d1
Ay Has <ahashim983@yahoo.com>
2019-03-09 16:45:35 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,6 +24,7 @@ def won?(board)
   WIN_COMBINATIONS.each do |single_combination|
     all_X = single_combination.all? do |each_num|
       index = each_num.to_i
+      puts index
       position_taken?(board,index)
     puts all_X
     end
