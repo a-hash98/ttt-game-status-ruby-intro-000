225a4c7237e77167596a360f5be8145d0348105c
Ay Has <ahashim983@yahoo.com>
2019-03-09 16:57:33 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -22,8 +22,10 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |single_combination|
-    all_X = single_combination.all? do |each_num|
-      puts(position_taken?(board,each_num))
+    all_xs = single_combination.select{|board[i]| board[i].is_a?("X")}
+        
+
+      #puts(position_taken?(board,each_num))
 
     end
   end
